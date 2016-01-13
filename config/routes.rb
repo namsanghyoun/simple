53c022f010df4924e99cc3620a7ee812c31a316b
nam <skatkdgus7@daum.net>
2016-01-13 13:28:03 +0000

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  match ':controller(/:action(/:id))', via: [:get, :post, :patch] 
-
+  get "home/index", :to => "home#index"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
